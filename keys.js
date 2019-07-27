2a0becd4a066f7b9bf855401414795d5dbc437c2
anees05 <anees.adams05@gmail.com>
2019-07-27 11:34:52 -0400

    Function for spotify command

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 // console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "0e5bba3e1a564477ba72eee52954b8ca",
+  secret: "91526cd1136c42aa9e1471e604a3c288"
 };
--- liri.js
+++ liri.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
-var Spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs");
 var moment = require("moment");
@@ -37,14 +36,15 @@ function runLiri(command, userSearch) {
 function getBandsInTown(artist) {
     var artist = userSearch;
     var bandQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
+ 
+   
     axios.get(bandQueryURL).then(
         function (response) {
             console.log("===================");
-            // console.log(response);
-            console.log("Name of the Venue " + response.data[0].venue.name + "\r\n");
-            console.log("Venue location " + response.data[0].venue.city + "\r\n");
-            console.log("Date of event " + moment(response.date[0].datetime).format("MM-DD-YYYY") + "\r\n");
+            console.log(response);
+            // console.log("Name of the Venue " + response.data[0].venue.name + "\r\n");
+            // console.log("Venue location " + response.data[0].venue.city + "\r\n");
+            // console.log("Date of event " + moment(response.data[0].datetime).format("MM-DD-YYYY") + "\r\n");
 
             var logConcert = "=====Concert Log Entry=====" + "\nName of the musician " + artist + "\nName of the venue " + venue + "\nName of city " + city + "\nDate of event " + date
 
@@ -79,10 +79,37 @@ function getOMB(movie) {
 
             var logMovie = "====Movie Log Entry====" + "\nMovie title: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nIMDB rating: " + response.data.imdbRating + "\nRotten Tomatoes rating: " + response.data.Ratings[1].Value + "\Country of production: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors;
 
-            fs.appendFile("log.txt", logMovie, function (err){
+            fs.appendFile("log.txt", logMovie, function (err) {
                 if (err) throw (err);
             });
         });
 };
 
+function getSpotify(songName) {
+    var spotify = new Spotify(keys.spotify);
+    console.log("Spotify key: " + spotify);
+
+    if(!songName) {
+        songName = "The Sign";
+    };
+
+    spotify.search({ type: "track", query: songName }, function (err, data){
+        if (err) {
+            return console.log("Error" + err);
+        }
+        console.log("Data for searching song: " + data.tracks.items[0]);
+
+        console.log("================");
+        console.log("Artist(s) Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
+        console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+        console.log("Song Preview Link: " + data.tracks.items[0].href + "\r\n");
+        console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
+        
+        var logSong = "=====Spotify Log Entry=====" + "\nArtist(s): " + data.tracks.items[0].albumt.artist[0].name + "\nSong Name: " + data.tracks.items[0].name + "\nSong Link: " + data.tracks.items[0].href + "\nAlbum: " + data.tracks.items[0].album.name
+        
+        fs.appendFile("log.txt", logSong, function (err){
+            if (err) throw err;
+        });
+    });
+}
 runLiri(command, userSearch);
