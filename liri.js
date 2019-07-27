1236696877cfcfd52c6335c74f47ad151be9a4ef
anees05 <anees.adams05@gmail.com>
2019-07-26 20:54:45 -0400

    Create variables

--- liri.js
+++ liri.js
@@ -3,3 +3,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new spotify(keys.spotify);
 
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+
+var command = process.argv[2];
+var userSearch = process.argv.slice(3).join(" ");
+
